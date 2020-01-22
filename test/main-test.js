da716a8715d6625366c24cd1975fa3b3cf9d169c
katerina <katpaltoglou@gmail.com>
2020-01-22 15:58:04 +0200

    update branch

--- test/main-test.js
+++ test/main-test.js
@@ -1,17 +1,39 @@
-import { expect } from "./util/expect";
-import sinon from "sinon";
-import * as Stage from "../lib/";
+var _expect = require("./util/expect");
 
-it('static methods', function() {
-  expect(Stage.config).be.a('function');
-  expect(Stage.preload).be.a('function');
-  expect(Stage.start).be.a('function');
-  expect(Stage.pause).be.a('function');
-  expect(Stage.resume).be.a('function');
-  expect(Stage.app).be.a('function');
-  expect(Stage.atlas).be.a('function');
+var _sinon = require("sinon");
 
-  expect(Stage.create).be.a('function');
+var _sinon2 = _interopRequireDefault(_sinon);
+
+var _lib = require("../lib/");
+
+var Stage = _interopRequireWildcard(_lib);
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+      }
+    }newObj.default = obj;return newObj;
+  }
+}
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+it('static methods', function () {
+  (0, _expect.expect)(Stage.config).be.a('function');
+  (0, _expect.expect)(Stage.preload).be.a('function');
+  (0, _expect.expect)(Stage.start).be.a('function');
+  (0, _expect.expect)(Stage.pause).be.a('function');
+  (0, _expect.expect)(Stage.resume).be.a('function');
+  (0, _expect.expect)(Stage.app).be.a('function');
+  (0, _expect.expect)(Stage.atlas).be.a('function');
+
+  (0, _expect.expect)(Stage.create).be.a('function');
 
   // expect(Stage.stage).be.a('function');
   // expect(Stage.image).be.a('function');
